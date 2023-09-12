c413119feae1d79b59f7c19cd1bb3a5c6af89dd8
Eli Uriegas <eliuriegas@meta.com>
2023-09-12 13:42:42 -0700

    add more mason lsp servers
    Signed-off-by: Eli Uriegas <eliuriegas@meta.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,13 +2,22 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
+      "black",
+      "clangd",
+      "eslint-lsp",
+      "json-lsp",
       "lua-language-server",
       "luacheck",
+      "luacheck",
       "luaformatter",
+      "python-lsp-server",
       "rust-analyzer",
       "shellcheck",
       "shfmt",
       "stylua",
+      "terraform-ls",
+      "tflint",
+      "typescript-language-server",
     },
   },
 }
